a542df22c1fca4436b1984fb52a1540ea52f4d09
Mo0nbupp <mel.aburto06@gmail.com>
2025-12-13 18:22:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const malla = document.getElementById("malla");
+
+const ejemplo = ["Sem 1", "Sem 2", "Sem 3"];
+
+ejemplo.forEach(texto => {
+  const div = document.createElement("div");
+  div.className = "ramo";
+  div.innerText = texto;
+  malla.appendChild(div);
+});
 
